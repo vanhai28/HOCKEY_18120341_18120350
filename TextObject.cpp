273f86a4c59d72f61a3f4b3108441bd644b044bf
nguyễn văn hải <48937890+vanhai28@users.noreply.github.com>
2019-10-19 17:31:06 +0700

    Create TextObject.cpp
    Định nghĩa hàm trong lớp textObject

---
+++ TextObject.cpp
@@ -0,0 +1,39 @@
+#include "TextObject.h"
+
+
+
+TextObject::TextObject()
+{
+}
+
+
+TextObject::~TextObject()
+{
+
+}
+
+void TextObject::SetColor(const int & type)
+{
+	SDL_Color  color;
+
+	if (type == RED_TEXT)
+	{
+		color = { 255, 0, 0 };
+	}
+	else if (type == WHITE_TEXT)
+	{
+		color = { 255, 255, 255 };
+	}
+	else
+	{
+		color = { 0, 0, 0 };
+	}
+
+	text_color = color;
+}
+
+void TextObject::creatText(TTF_Font * font, SDL_Surface *des)
+{
+	p_object_ = TTF_RenderText_Solid(font, str_val_.c_str(), text_color);
+	show(des);
+}
