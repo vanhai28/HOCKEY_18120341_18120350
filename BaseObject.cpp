7bd87979f1628245f430aa37ca6d1b594da07484
nguyễn văn hải <48937890+vanhai28@users.noreply.github.com>
2019-10-19 17:34:02 +0700

    Create BaseObject.cpp
    Lớp cha của các đối tượng trong game

---
+++ BaseObject.cpp
@@ -0,0 +1,26 @@
+#include "BaseObject.h"
+
+
+
+BaseObject::BaseObject()
+{
+	rect_.x = 0;
+	rect_.y = 0;
+	p_object_ = NULL;
+}
+
+
+BaseObject::~BaseObject()
+{
+}
+
+bool BaseObject::LoadImg(const char* file_name) {
+	p_object_ = SDL_CFunction::LoadImage(file_name);
+	return p_object_ != NULL;
+}
+
+void BaseObject::show(SDL_Surface * des) {
+	if (p_object_ != NULL) {
+		SDL_CFunction::ApplySurface(p_object_, des, rect_.x, rect_.y);
+	}
+}
