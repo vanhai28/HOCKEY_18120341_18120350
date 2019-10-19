026f47b1bdfd942510b446d3f430e3650f21a1fa
nguyễn văn hải <48937890+vanhai28@users.noreply.github.com>
2019-10-19 17:31:44 +0700

    Create TextObject.h
    Khai báo lớp TextObject

---
+++ TextObject.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "BaseObject.h"
+#include <string>
+class TextObject : public BaseObject
+{
+public:
+	enum TextColor {
+		RED_TEXT = 0,
+		WHITE_TEXT = 1,
+		BLACK_TEXT = 2
+	};
+	TextObject();
+	~TextObject();
+
+	void SetText(const std::string text) { str_val_ = text; }
+	void SetColor(const int &type);
+	void creatText(TTF_Font *font, SDL_Surface* des);
+
+
+
+private:
+	std::string str_val_;
+	SDL_Color text_color;
+
+};
+
