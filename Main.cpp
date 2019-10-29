3f24121ad73625b32a1f9681356081a43dbeba68
nguyễn văn hải <48937890+vanhai28@users.noreply.github.com>
2019-10-29 14:32:01 +0700

    Update Main.cpp
    update

--- Main.cpp
+++ Main.cpp
@@ -1,4 +1,4 @@
-#include <Windows.h>
+
 #include <SDL.h>
 #include "CommonFunction.h"
 #include <SDL_ttf.h>
@@ -7,9 +7,8 @@
 #include "Player1.h"
 #include "Player2.h"
 #include "Ball.h"
-#include <SDL_ttf.h>
 #include "TextObject.h"
-#define MOVE_SPEED 100.0f
+#include <SDL_mixer.h>
 
 #undef main
 using namespace std;
@@ -25,13 +24,14 @@ Ball ball;
 bool is_finish_game = false;
 int winner = 0;
 //------------------------------------------------
-
+void showWinner(SDL_Surface*& screen);
 bool Init();
 bool startGame();
 void pause_game(SDL_Surface*& screen);
 void restartGame(); 
 void ExitGame();
 
+Mix_Music *music = NULL;
 
 int main()
 {
@@ -48,16 +48,45 @@ int main()
 	text_object.SetRect(400, 400);
 	text_object.SetColor(TextObject::BLACK_TEXT);
 	text_object.SetText("in cho vui");
-	
+	text_object.creatText(g_font_text, g_screen);
+	int ret_menu = SDL_CFunction::ShowMenu(g_screen, g_font_text);
+
+	if (ret_menu == 1)
+	{
+		is_quit = true;
+	}
+
 	while (!is_quit)
 	{
+		
+		if (Mix_PlayingMusic() == 0)
+		{
+			//Play the music
+			Mix_PlayMusic(music, -1);
+		}
+
 		SDL_CFunction::ApplySurface(g_bkground, g_screen, 0, 0);
-		text_object.creatText(g_font_text, g_screen);
+		//Mix_PlayChannel(-1, g_sound_player1, 0);
 		while (SDL_PollEvent(&g_event))
 		{
 			if (g_event.type == SDL_QUIT) {
 				is_quit = true;
 				break;
+			}
+
+			if (g_event.type == SDL_KEYDOWN)
+			{
+				switch (g_event.key.keysym.sym)
+					{
+					case SDLK_SPACE:
+					{
+						pause_game(g_screen);
+					}
+					case SDLK_n:
+					{
+						
+					}
+				}
 			}
 
 			player2.HandleInputAction(g_event);
@@ -70,11 +99,12 @@ int main()
 		player2.HandleMove();
 		player2.show(g_screen);
 
-		ball.HandleMove(player1.GetRect(), player2.GetRect(), is_finish_game, winner);
+		ball.HandleMove(player1.GetRect(), player2.GetRect(), is_finish_game, winner,g_sound_player1);
 		ball.show(g_screen);
 	
 		if(is_finish_game)
 		{
+			showWinner(g_screen);
 			pause_game(g_screen);
 		}
 
@@ -83,13 +113,17 @@ int main()
 		}
 	}
 	ExitGame();
-
+	
+	Mix_FreeChunk(g_sound_player1);
+	Mix_FreeChunk(win);
+	Mix_FreeMusic(music);
+	Mix_Quit();
 	return 0;
 }
 
 bool Init()
 {
-	if (SDL_Init(SDL_INIT_EVERYTHING) == -1) {
+	if (SDL_Init(SDL_INIT_EVERYTHING|| SDL_INIT_VIDEO | SDL_INIT_AUDIO) == -1) {
 		cerr << "SDL_Init() Failed: " <<
 			SDL_GetError() << endl;
 		return false;
@@ -105,7 +139,24 @@ bool Init()
 	if (TTF_Init() < 0)
 	{
 		cout<<"Loi khoi tao TTF:"<< TTF_GetError();
-		return -1;
+		return false;
+	}
+
+	if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0)
+	{
+		return false;
+	}
+
+	g_sound_player1 = Mix_LoadWAV("palyer2.wav");
+	win = Mix_LoadWAV("winSound.wav");
+
+	music = Mix_LoadMUS("Vexento - We Are One (Original).mp3");
+	
+
+	if ( g_sound_player1 == NULL || music == NULL|| win == NULL)
+	{
+		cout << "cannot open file audio\n";
+		return false;
 	}
 
 	return true;
@@ -185,19 +236,20 @@ void pause_game(SDL_Surface*& screen)
 				}
 			}
 		}
-		if (winner == 1) {
-			win1.show(screen);
-		}
-		else {
-			win2.show(screen);
-		}
-		if (SDL_Flip(screen) == -1) {
-			return;
-		}
-
 	}
 }
-
+void showWinner(SDL_Surface*& screen)
+{
+	if (winner == 1) {
+		win1.show(screen);
+	}
+	else {
+		win2.show(screen);
+	}
+	if (SDL_Flip(screen) == -1) {
+		return;
+	}
+}
 void restartGame()
 {
 	player2.SetRect(X_PLAYER_2, Y_PLAYER_2);
