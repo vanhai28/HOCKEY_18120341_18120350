c1eb5cab999dcd7f9fd49006d047b707762237f8
nguyễn văn hải <48937890+vanhai28@users.noreply.github.com>
2019-10-29 14:31:36 +0700

    Update CommonFunction.h

--- CommonFunction.h
+++ CommonFunction.h
@@ -1,5 +1,4 @@
 #pragma once
-
 #include <windows.h>
 #include <SDL_image.h>
 #include <SDL.h>
@@ -7,24 +6,27 @@
 #include <SDL_ttf.h>
 #include <SDL_image.h>
 
+
 const int SCREEN_WIDTH = 900;
 const int SCREEN_HEIGHT = 700;
 const int SCREEN_BPP = 32;
 const int RIGHT_BOUNDARY = 545;
 const int LEFT_BOUNDARY = 5;
 const int TOP_BOUNDARY = 5;
-const int BOTTOM_BOUNDRY = SCREEN_HEIGHT ;
+const int BOTTOM_BOUNDRY = SCREEN_HEIGHT;
 
 
 static SDL_Surface * g_screen = NULL;
 static SDL_Surface * g_bkground = NULL;
+static SDL_Surface* g_img_menu = NULL;
 static SDL_Event g_event;
 
 namespace SDL_CFunction {
 	void   CleanUp();
-	void   ApplySurface(SDL_Surface*& scr, SDL_Surface*& des, int x, int y);
+	SDL_Rect  ApplySurface(SDL_Surface* scr, SDL_Surface* des, int x, int y);
 
 	SDL_Surface*   LoadImage(std::string file_path);
-}
 
-void pause_game();
+	int ShowMenu(SDL_Surface *des, TTF_Font * font);
+	bool CheckFocusWithRect(const int &x, const int &y, const SDL_Rect& rect);
+}
