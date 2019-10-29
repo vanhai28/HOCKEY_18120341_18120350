32342a21ee06efb1ff938451ff9602c33a29f90b
nguyễn văn hải <48937890+vanhai28@users.noreply.github.com>
2019-10-29 14:29:45 +0700

    Update Ball.h
    update

--- Ball.h
+++ Ball.h
@@ -4,15 +4,20 @@
 #include "Player1.h"
 #include "Player2.h"
 #include "CommonFunction.h"
+#include <SDL_mixer.h>
 #define WIDTH_BALL 40
 #define HEIGHT_BALL 31
+#define MAX_SPEED 10
+//-------------------static variable---------------
+static Mix_Chunk* g_sound_player1 = NULL;
+static Mix_Chunk* win = NULL;
 
 
 
 class Ball : public   BaseObject
 {
 public:
-	void HandleMove(SDL_Rect playerTop, SDL_Rect playerButtom, bool &is_finish, int &win);
+	void HandleMove(SDL_Rect playerTop, SDL_Rect playerButtom, bool &is_finish, int &win,Mix_Chunk*& bullet_sound);
 
 	int Get_x_val() { return x_val; }
 	void Set_x_val(int val) { x_val = val; }
