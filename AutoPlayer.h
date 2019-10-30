81a441d0e21406c2e24d1589a4a3a51ac7a01d3c
nguyễn văn hải <48937890+vanhai28@users.noreply.github.com>
2019-10-30 16:19:00 +0700

    Update AutoPlayer.h

--- AutoPlayer.h
+++ AutoPlayer.h
@@ -7,7 +7,7 @@ public:
 	~AutoPlayer();
 
 	void PredictDropPoint(SDL_Rect, int, int);
-	void  HandleMove(SDL_Rect ball, int x_ball_val, int y_ball_val);
+	void  HandleMove();
 	bool Get_is_move() { return is_move; }
 	void Set_is_move(bool val) { is_move = val; }
 
