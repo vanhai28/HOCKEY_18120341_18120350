71dcabe59b0873e6b6fc0036fa5ad5764d226dd9
nguyễn văn hải <48937890+vanhai28@users.noreply.github.com>
2019-10-30 23:15:09 +0700

    Update Button.cpp

--- Button.cpp
+++ Button.cpp
@@ -2,7 +2,7 @@
 
 bool Button::CheckFocusWithRect(const int &x, const int &y, const SDL_Rect& rect)
 {
-	if (x >= rect.x && x <= rect.x + rect.w && y >= rect.y && y <= rect.y + rect.h)
+	if (x >= rect.x && x <= rect.x + rect.w && y >= rect.y + 5 && y <= rect.y + rect.h)
 	{
 		return true;
 	}
@@ -11,43 +11,35 @@ bool Button::CheckFocusWithRect(const int &x, const int &y, const SDL_Rect& rect
 }
 
 
-bool Button::SetColorButton(SDL_Event _event, SDL_Surface *des)
+void Button::UpdateColorButton(SDL_Event _event, SDL_Surface *des)
 {
-	if (TTF_Init() < 0)
-	{
-		return false;
-	}
-
 	TTF_Font* font = TTF_OpenFont("comic.ttf", 60);
 
 	int x_mouse = _event.motion.x;
 	int y_mouse = _event.motion.y;
 
-	switch (_event.type)
+	if (_event.type == SDL_MOUSEMOTION)
 	{
-		case SDL_MOUSEMOTION:
+		//text_menu[i].SetColor(TextObject::RED_TEXT);
+		if (CheckFocusWithRect(x_mouse, y_mouse, this->GetRect()))
 		{
-			//text_menu[i].SetColor(TextObject::RED_TEXT);
-			if (CheckFocusWithRect(x_mouse, y_mouse, this->GetRect()))
-			{
-				this->SetColor(TextObject::RED_TEXT);
-				this->creatText(font, des);
-			}
-			else
-			{
-				this->SetColor(TextObject::BLACK_TEXT);
-				this->creatText(font, des);
-			}
-			break;
+			this->SetColor(TextObject::RED_TEXT);
+			this->creatText(font, des);
+		}
+		else
+		{
+			this->SetColor(TextObject::BLACK_TEXT);
+			this->creatText(font, des);
 		}
 	}
 
 	TTF_CloseFont(font);
-	return false;
+	return ;
 }
 
 Button::Button()
 {
+
 	SetColor(TextObject::BLACK_TEXT);
 }
 
