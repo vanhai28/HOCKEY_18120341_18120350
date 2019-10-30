2abc792d2850517c6994ad65f4eed4f49de01937
nguyễn văn hải <48937890+vanhai28@users.noreply.github.com>
2019-10-30 14:01:37 +0700

    Update CommonFunction.cpp

--- CommonFunction.cpp
+++ CommonFunction.cpp
@@ -1,13 +1,29 @@
 ﻿
 #include "CommonFunction.h"
+#include <iostream>
 #include"TextObject.h"
+/*
+ * Function:
+ * ----------------------------------
+ *	Free memory
 
+ */
 void SDL_CFunction::CleanUp()
 {
 	SDL_FreeSurface(g_screen);
 	SDL_FreeSurface(g_bkground);
 }
 
+/*
+ * Function: ApplySurface
+ * ----------------------------
+ *	 Apply the source of surface to detination of surface
+ *
+ *	 scr: a pointer point to source of Surface
+ *   des : a pointer point to destination of Surface
+ *
+ *   Return coordinates of the surface destination
+ */
 SDL_Rect SDL_CFunction::ApplySurface(SDL_Surface* scr, SDL_Surface* des, int x, int y)
 {
 	SDL_Rect offset;
@@ -17,15 +33,35 @@ SDL_Rect SDL_CFunction::ApplySurface(SDL_Surface* scr, SDL_Surface* des, int x, 
 
 	return offset;
 }
-//description: kiểm tra tọa độ.
+
+/*
+ * Function: CheckFocusWithRect
+ * ----------------------------------
+ *	x :
+ *	y :
+ *	rect :
+ *
+ *   Return
+ */
 bool SDL_CFunction::CheckFocusWithRect(const int &x, const int &y, const SDL_Rect& rect)
 {
 	if (x >= rect.x && x <= rect.x + rect.w && y >= rect.y && y <= rect.y + rect.h)
 	{
 		return true;
 	}
+
 	return false;
 }
+
+
+/*
+ * Function:LoadImage
+ * ----------------------------------
+ *	Load the file of image
+ *	file_path: the path to file on the computer including the name of the file
+ *
+ *   Return an SDL_Surface pointer point to the surface of the image
+ */
 SDL_Surface*  SDL_CFunction::LoadImage(std::string file_path) {
 	SDL_Surface* load_image = NULL;
 	SDL_Surface *optimize_image = NULL;
@@ -47,80 +83,89 @@ SDL_Surface*  SDL_CFunction::LoadImage(std::string file_path) {
 	return optimize_image;
 }
 
-//description: vẽ menu và lựa chọn cách chơi game.
+/*
+ * Function:ShowMenu
+ * ----------------------------------
+ *	Display the menu of the program when it begin
+ *
+ *	des: The Surface to apply image background
+ *	font: the font of the text to show on screen
+ *
+ *   Return 0: if the user click to 2 player button
+ *   Return 2: if the user click to play with PC
+ *   Return 2: if this has the error or user click quit button
+ *
+ */
+
 int SDL_CFunction::ShowMenu(SDL_Surface *des, TTF_Font * font)
 {
-	g_img_menu = LoadImage("menu.png");
-	
-	if (g_img_menu == NULL)	//kiểm tra có ảnh menu hay không.
+	g_img_menu = LoadImage("Bkground.png");
+
+	if (g_img_menu == NULL)
 	{
-		return 1;
+		return 2;
 	}
-
+	//số chức năng chơi game.
 	const int kMenuItemNum = 3;
-	//tạo mảng để lưu 3 tọa độ của 3 chức năng cần sử dụng.
-	SDL_Rect pos_Arr[kMenuItemNum];
-	//chơi 1 vs 1.
-	pos_Arr[0].x = 100;
-	pos_Arr[0].y = 300;
-	//chơi 1 vs may.
-	pos_Arr[1].x = 100;
-	pos_Arr[1].y = 500;
-	//thoát game.
-	pos_Arr[2].x = 100;
-	pos_Arr[2].y = 400;
-
 	TextObject text_menu[kMenuItemNum];
 
-	//vẽ các yêu cầu lên menu.png.
 	text_menu[0].SetText("Play Game: 1 vs 1");
 	text_menu[0].SetColor(TextObject::BLACK_TEXT);
-	text_menu[0].SetRect(pos_Arr[0].x, pos_Arr[0].y);
+	text_menu[0].SetRect(100,300,458,60);
 
-	text_menu[2].SetText("Play Game: 1 vs may");
-	text_menu[2].SetColor(TextObject::BLACK_TEXT);
-	text_menu[2].SetRect(pos_Arr[2].x, pos_Arr[2].y);
-
-	text_menu[1].SetText("EXIT GAME");
+	text_menu[1].SetText("PLay Game: 1 vs may");
 	text_menu[1].SetColor(TextObject::BLACK_TEXT);
-	text_menu[1].SetRect(pos_Arr[1].x, pos_Arr[1].y);
+	text_menu[1].SetRect(100,400,558,60);
 
-	bool selected[kMenuItemNum] = { 0,0 };
+	text_menu[2].SetText("EXIT GAME");
+	text_menu[2].SetColor(TextObject::BLACK_TEXT);
+	text_menu[2].SetRect(100,500,350,60);
+
+	bool selected[kMenuItemNum] = { 0,0,0 };
 	int xm = 0;
 	int ym = 0;
 	SDL_Event m_event;
+	SDL_CFunction::ApplySurface(g_img_menu, des, 0, 0);
+	for (int i = 0; i < kMenuItemNum; i++)
+	{
+		//text_menu[i].SetColor(TextObject::RED_TEXT);
+		text_menu[i].creatText(font, des);
+	}
 
 	while (true)
 	{
-		SDL_CFunction::ApplySurface(g_img_menu, des, 0, 0);
-		for (int i = 0; i < kMenuItemNum; ++i)
-		{
-			text_menu[i].creatText(font, des);
-		}
-
+		
 		while (SDL_PollEvent(&m_event))
 		{
 			switch (m_event.type)
 			{
 			case SDL_QUIT:
-			{ return 1; }
+			{
+				return 2;
+			}
 			case SDL_MOUSEMOTION:
 			{
 				xm = m_event.motion.x;
 				ym = m_event.motion.y;
-
+				std::cout <<"x =  "<<xm<<"  y =  "<< ym;
 				for (int i = 0; i < kMenuItemNum; i++)
 				{
-					if (CheckFocusWithRect(xm, ym, text_menu[i].GetRect()))		//nêu con trỏ chuột nằm trong vi trí 
-					{															//của các yêu cầu thì các yêu cầu chuyển sang mau đỏ
+					//text_menu[i].SetColor(TextObject::RED_TEXT);
+					if (CheckFocusWithRect(xm, ym, text_menu[i].GetRect()))
+					{
+						text_menu[i].SetColor(TextObject::RED_TEXT);
+						text_menu[i].creatText(font, des);
 						if (selected[i] == false)
 						{
 							selected[i] = 1;
 							text_menu[i].SetColor(TextObject::RED_TEXT);
 						}
+					
 					}
 					else
 					{
+						text_menu[i].SetColor(TextObject::BLACK_TEXT);
+						text_menu[i].creatText(font, des);
 						if (selected[i] == true)
 						{
 							selected[i] = 0;
@@ -128,8 +173,9 @@ int SDL_CFunction::ShowMenu(SDL_Surface *des, TTF_Font * font)
 						}
 					}
 				}
+				break;
 			}
-			break;
+
 			case SDL_MOUSEBUTTONDOWN:
 			{
 				xm = m_event.button.x;
@@ -139,22 +185,22 @@ int SDL_CFunction::ShowMenu(SDL_Surface *des, TTF_Font * font)
 				{
 					if (CheckFocusWithRect(xm, ym, text_menu[i].GetRect()))
 					{
-						return i;		// return 0 là chơi với người, return 1 là chơi với máy
-					}					// return 2 là exit;
+						return i;
+					}
 				}
 			}
 			break;
 			case SDL_KEYDOWN:
 				if (m_event.key.keysym.sym == SDLK_ESCAPE)
 				{
-					return 1;
+					return 2;
 				}
 			default:
 				break;
 			}
 		}
-		SDL_Flip(des);
+		if (SDL_Flip(des) == -1) {
+			return 2;
+		}
 	}
-
-	return 1;
 }
