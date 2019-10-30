619cbdc37364b205e22529825d6fe4dd64eee31a
nguyễn văn hải <48937890+vanhai28@users.noreply.github.com>
2019-10-30 16:19:57 +0700

    Update Player2.h

--- Player2.h
+++ Player2.h
@@ -1,15 +1,16 @@
 #pragma once
-#include "Player1.h"
+#include "PLAYER.h"
 
 const int  X_PLAYER_2 = RIGHT_BOUNDARY / 2;
 const int  Y_PLAYER_2 = SCREEN_HEIGHT - 30;
 
-class Player2 : public Player1
+class Player2 : public PLAYER
 {
 public:
 	Player2();
 
 	~Player2();
+	void PredictDropPoint(SDL_Rect a, int b, int c) {};
 	void HandleInputAction(SDL_Event events);
 	void HandleMove();
 	bool Get_is_move() { return is_move; }
