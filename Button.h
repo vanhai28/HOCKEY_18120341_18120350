0808d9bbc4245e141bfca79b700e312e497ca735
nguyễn văn hải <48937890+vanhai28@users.noreply.github.com>
2019-10-31 23:29:01 +0700

    Update Button.h

--- Button.h
+++ Button.h
@@ -3,7 +3,7 @@
 class Button : public TextObject
 {
 public:
-	void UpdateColorButton(SDL_Event _event, SDL_Surface *des);
+	void UpdateColorButton(SDL_Event& _event, SDL_Surface *& des);
 	bool CheckFocusWithRect(const int &x, const int &y, const SDL_Rect& rect);
 
 	Button();
