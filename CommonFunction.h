a08b292eee8c3175a5a3f69633f3f641d8c3e286
nguyễn văn hải <48937890+vanhai28@users.noreply.github.com>
2019-10-31 23:29:20 +0700

    Update CommonFunction.h

--- CommonFunction.h
+++ CommonFunction.h
@@ -5,15 +5,14 @@
 #include <string>
 #include <SDL_ttf.h>
 #include <SDL_image.h>
-//#include "Button.h"
 
 const int SCREEN_WIDTH = 900;
 const int SCREEN_HEIGHT = 700;
 const int SCREEN_BPP = 32;
-const int RIGHT_BOUNDARY = 545;
+const int RIGHT_BOUNDARY = 500;
 const int LEFT_BOUNDARY = 5;
 const int TOP_BOUNDARY = 5;
-const int BOTTOM_BOUNDRY = SCREEN_HEIGHT;
+const int BOTTOM_BOUNDRY = SCREEN_HEIGHT - 5;
 
 
 static SDL_Surface * g_screen = NULL;
@@ -24,7 +23,6 @@ static SDL_Event g_event;
 namespace SDL_CFunction {
 	void   CleanUp();
 	SDL_Rect  ApplySurface(SDL_Surface* scr, SDL_Surface* des, int x, int y);
-
-	SDL_Surface*   LoadImage(std::string file_path);
-
+	SDL_Surface* LoadImage(std::string file_path);
 }
+int ShowMenu(SDL_Surface *des, TTF_Font * font);
