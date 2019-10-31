b45267e126aabb3c84f78fab3a35c9e8f7a3f697
nguyễn văn hải <48937890+vanhai28@users.noreply.github.com>
2019-10-31 23:29:47 +0700

    Update PLAYER.h

--- PLAYER.h
+++ PLAYER.h
@@ -4,15 +4,29 @@
 const int  X_PLAYER = RIGHT_BOUNDARY / 2;
 const int  Y_PLAYER = 20;
 
-#define WIDTH_PLAYER 88
-#define HEIGHT_PLAYER 30
+#define WIDTH_PLAYER 120
+#define HEIGHT_PLAYER 15
 class PLAYER :public BaseObject
 {
 public:
 	virtual void HandleInputAction(SDL_Event events) = 0;
 	virtual void HandleMove() = 0;
 	virtual void PredictDropPoint(SDL_Rect, int, int) = 0;
+
+	bool GetIsMove() { return is_move; }
+	void SetIsMove(bool val) { is_move = val; }
+
+	int GetMark() { return mark; }
+	void SetMark(int val) { mark = val; }
+
 	PLAYER();
 	~PLAYER();
+
+
+protected :
+	bool is_move;
+	int x_val;
+	int y_val;
+	int mark;
 };
 
