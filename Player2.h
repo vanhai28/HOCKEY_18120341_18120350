d1acf58ed2410016253153f11bdc239d65f6936a
nguyễn văn hải <48937890+vanhai28@users.noreply.github.com>
2019-11-01 15:46:47 +0700

    Update Player2.h

--- Player2.h
+++ Player2.h
@@ -1,20 +1,26 @@
+﻿// Mô tả : Định nghĩa lớp người chơi thứ hai
+// Ngày cập nhật : 01/11/2019
+
 #pragma once
 #include "PLAYER.h"
 
-const int  X_PLAYER_2 = RIGHT_BOUNDARY / 2;
+const int  X_PLAYER_2 = RIGHT_BOUNDARY / 2 - WIDTH_PLAYER /2;
 const int  Y_PLAYER_2 = SCREEN_HEIGHT - 30;
 
 class Player2 : public PLAYER
 {
 public:
 	Player2();
-
 	~Player2();
+	//-------------- Định nghĩa các hàm ảo từ lớp cơ sở --------------
+
 	void PredictDropPoint(SDL_Rect a, int b, int c) {};
+
 	void HandleInputAction(SDL_Event events);
+
 	void HandleMove();
 
-	
+	//----------------------------------------------------------------
 
 };
 
