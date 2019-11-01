d54f42e5a789f64f46c72558c0b49bcfb3bbec6e
nguyễn văn hải <48937890+vanhai28@users.noreply.github.com>
2019-11-01 15:46:51 +0700

    Update Player1.h

--- Player1.h
+++ Player1.h
@@ -1,3 +1,6 @@
+﻿// Mô tả : Định nghĩa lớp người chơi thứ nhất
+// Ngày cập nhật : 01/11/2019
+
 #pragma once
 #include <SDL_mixer.h>
 #include "PLAYER.h"
@@ -7,11 +10,13 @@ class Player1 : public PLAYER
 public:
 	Player1();
 	~Player1();
-
+	//-------------- Định nghĩa các hàm ảo từ lớp cơ sở --------------
 	void HandleInputAction(SDL_Event events);
+
 	void  HandleMove();
+
 	void PredictDropPoint(SDL_Rect a, int b, int c) {};
 
-
+	//----------------------------------------------------------------
 };
 
