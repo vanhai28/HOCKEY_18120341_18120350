c5de4a991b5802732810c24424298af80917b30c
nguyễn văn hải <48937890+vanhai28@users.noreply.github.com>
2019-11-01 15:46:49 +0700

    Update TextObject.h

--- TextObject.h
+++ TextObject.h
@@ -1,6 +1,11 @@
+﻿// Mô tả : Định nghĩa lớp TextObject, hiển thị kí tự lên màn hình
+// Ngày cập nhật : 01/11/2019
+
 #pragma once
 #include "BaseObject.h"
 #include <string>
+
+
 class TextObject : public BaseObject
 {
 public:
@@ -11,13 +16,20 @@ public:
 	};
 	TextObject();
 	~TextObject();
-	
+
+	//--------------Các hàm GETTER- SETTER -------------
 	std::string GetText() { return str_val_; }
 	void SetText(const std::string text) { str_val_ = text; }
 
 	SDL_Color GetColor() {return text_color;}
 	void SetColor(const int &type);
+	//--------------------------------------------------
 
+	// Tên hàm 	: creatText
+	// Mô tả	: hiển thị chữ lên màn hình
+	// Kiểu trả về  : void 
+	// Tham số TTF_Font *font: Font chữ hiển thị
+	// Tham số SDL_Surface* des : đối tượng đích sẽ hiển thị lên, thường là cửa sổ người chơi
 	void creatText(TTF_Font *font, SDL_Surface* des);
 
 private:
