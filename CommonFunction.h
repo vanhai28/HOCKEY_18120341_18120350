aa7cb0717f1124881c77616314cce8100f5774ab
nguyễn văn hải <48937890+vanhai28@users.noreply.github.com>
2019-11-01 15:47:51 +0700

    Update CommonFunction.h

--- CommonFunction.h
+++ CommonFunction.h
@@ -1,3 +1,6 @@
+﻿// Mô tả: Các hàm, biến dùng chung cho các lớp khác 
+// Ngày cập nhật : 01/11/2019
+
 #pragma once
 #include <windows.h>
 #include <SDL_image.h>
@@ -21,8 +24,34 @@ static SDL_Surface* g_img_menu = NULL;
 static SDL_Event g_event;
 
 namespace SDL_CFunction {
+
+	//-------------------------------------------------------------
+	// Tên hàm 	: CleanUp
+	// Mô tả	: Giải phóng bộ nhớ các con trỏ sử dụng
+	// Kiểu trả về : void
 	void   CleanUp();
+
+	//-------------------------------------------------------------
+	// Tên hàm     : ApplySurface
+	// Mô tả	   : Ðặt SDL_Surface nguồn vào SDL_SurFace dích (thường là màn hình)
+	// Kiểu trả về : SDL_Rect
+	// Tham số SDL_Surface* scr : con trỏ trỏ đến nguồn
+	// Tham số  SDL_Surface* des : con trỏ trỏ đến đích
+	// Tham số int x : Tọa độ vị trí đặt hình trên cửa sổ .
+	// Tham số int y : Tọa độ vị trí đặt hình trên cửa sổ .
 	SDL_Rect  ApplySurface(SDL_Surface* scr, SDL_Surface* des, int x, int y);
+
+	//-------------------------------------------------------------
+	// Tên hàm 	: LoadImage
+	// Mô tả : Tải hình lên
+	// Kiểu trả về : SDL_Surface*
+	// Tham số string file_path : Đường dẫn file
 	SDL_Surface* LoadImage(std::string file_path);
 }
+
+	// Tên hàm 	: ShowMenu
+	// Mô tả	: Hiển thị cửa sổ menu
+	// Kiểu trả về  : int, xác định chức năng người chơi chọn
+	// Tham số SDL_Surface *des : Biến đích (thường là màn hình)
+	// Tham số TTF_Font * font : Font chữ hiển thị
 int ShowMenu(SDL_Surface *des, TTF_Font * font);
