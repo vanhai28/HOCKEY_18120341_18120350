addb0faa4cc6c2003fba8b494371e25780f61299
nguyễn văn hải <48937890+vanhai28@users.noreply.github.com>
2019-11-01 15:47:48 +0700

    Update Button.h

--- Button.h
+++ Button.h
@@ -1,9 +1,26 @@
+﻿// Mô tả : Định nghĩa lớp button
+// Ngày cập nhật : 01/11/2019
+
+
 #pragma once
 #include "TextObject.h"
 class Button : public TextObject
 {
 public:
+
+	// Tên hàm 	: UpdateColorButton
+	// Mô tả	: Cập nhật lại màu hiển thị của text khi người chơi di chuyển chuột 
+	// Kiểu trả về  : void 
+	// Tham số SDL_Event& _event : Sự kiện nhận vào từ chuột
+	// Tham số SDL_Surface *& des : Đối tượng đích để hiển thị lên, thường là cửa sổ người chơi.
 	void UpdateColorButton(SDL_Event& _event, SDL_Surface *& des);
+
+	// Tên hàm 	: CheckFocusWithRect
+	// Mô tả	: Kiểm tra xem con trỏ chuột có di chuyển đến vị trí nút (button )
+	// Kiểu trả về  : bool
+	// Tham số onst int &x : Tọa độ x của con trỏ chuột
+	// Tham số const int &y : Tọa độ y của con trỏ chuột 
+	// Tham số  const SDL_Rect& rect : Tọa độ của nút 
 	bool CheckFocusWithRect(const int &x, const int &y, const SDL_Rect& rect);
 
 	Button();
