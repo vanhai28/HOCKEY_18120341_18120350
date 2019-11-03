19907f0e88dfe614424b57cc20af75897ab2830b
nguyễn văn hải <48937890+vanhai28@users.noreply.github.com>
2019-11-03 20:37:36 +0700

    Update AutoPlayer.cpp

--- AutoPlayer.cpp
+++ AutoPlayer.cpp
@@ -19,8 +19,8 @@ AutoPlayer::~AutoPlayer()
 
 void AutoPlayer::PredictDropPoint(SDL_Rect ball, int x_val_ball, int y_val_ball)
 {
-	if ( y_val_ball > 0 || ball.y > BOTTOM_BOUNDRY / 2)
-	{ 
+	if (y_val_ball > 0 || ball.y > BOTTOM_BOUNDRY / 2)
+	{
 		return;
 	}
 	int x = ball.x;
@@ -42,22 +42,22 @@ void AutoPlayer::PredictDropPoint(SDL_Rect ball, int x_val_ball, int y_val_ball)
 	}
 
 	is_move = true;
-	x_des = x;
+	x_ball_predict = x;
 }
 
 void AutoPlayer::HandleMove()
 {
-	if (is_move && rect_.x < x_des && x_des < (rect_.x + WIDTH_PLAYER))
+	if (is_move && rect_.x < x_ball_predict && x_ball_predict < (rect_.x + WIDTH_PLAYER))
 	{
 		is_move = false;
 	}
 	else if (is_move)
 	{
-		if (rect_.x + WIDTH_PLAYER <= x_des && rect_.x + WIDTH_PLAYER + x_step < RIGHT_BOUNDARY)
+		if (rect_.x + WIDTH_PLAYER <= x_ball_predict && rect_.x + WIDTH_PLAYER + x_step < RIGHT_BOUNDARY)
 		{
 			rect_.x += x_step;
 		}
-		else if (rect_.x >= x_des && rect_.x - x_step > LEFT_BOUNDARY)
+		else if (rect_.x >= x_ball_predict && rect_.x - x_step > LEFT_BOUNDARY)
 		{
 			rect_.x -= x_step;
 		}
