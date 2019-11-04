858dbcba0d552f66f6de306516cb29b9e782db69
nguyễn văn hải <48937890+vanhai28@users.noreply.github.com>
2019-11-04 11:45:32 +0700

    Update Ball.h

--- Ball.h
+++ Ball.h
@@ -1,17 +1,12 @@
-﻿// Mô t?: L?p ball d?nh nghi cho l?p qu? bóng trong game
-// Ngày c?p nh?t : 01/11/2019
+﻿#pragma once
 
-#pragma once
-
-#include "BasePlayer.h"
+#include "Player.h"
 #include <SDL_mixer.h>
 
 #define WIDTH_BALL 15
 #define HEIGHT_BALL 15
-#define MAX_SPEED 20
+#define MAX_SPEED 18
 //-------------------static variable---------------
-static Mix_Chunk* g_sound_player1 = NULL;
-static Mix_Chunk* win = NULL;
 
 
 
@@ -29,7 +24,7 @@ public:
 	// Tham số Mix_Chunk*& bullet_sound : Biến sử dụng tải âm thanh khi bóng cham thanh
 	// Tham số mark : Tăng điểm của người chơi trong màn chơi với máy
 
-	void HandleMove(SDL_Rect playerTop, SDL_Rect playerButtom, bool &is_finish, int &win, Mix_Chunk*& bullet_sound, int & mark);
+	int HandleMove(SDL_Rect playerTop, SDL_Rect playerButtom, bool &is_finish, Mix_Chunk* sound[3], int& mark);
 
 	//------------- Các hàm Getter - Setter------------
 	int Get_x_val() { return x_val; }
@@ -40,13 +35,16 @@ public:
 
 	bool Get_is_move() { return is_move; }
 	void Set_is_move(bool val) { is_move = val; }
+
+	int Get_speed() { return speed; }
+	void Set_speed(int val) { speed = val; }
 	//-------------------------------------------------
 
 	Ball();
 	~Ball();
 
 private:
-
+	int speed;
 	bool is_move;
 	int x_val;
 	int y_val;
