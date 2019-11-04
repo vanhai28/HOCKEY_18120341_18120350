57dbd833c2c7b81a06de03fc6887daa6e9d18af9
nguyễn văn hải <48937890+vanhai28@users.noreply.github.com>
2019-11-04 11:45:40 +0700

    Update BaseObject.h

--- BaseObject.h
+++ BaseObject.h
@@ -1,3 +1,4 @@
+﻿
 #pragma once
 #include <SDL.h>
 #include "CommonFunction.h"
@@ -8,11 +9,22 @@ public:
 	BaseObject();
 	~BaseObject();
 
+	// Tên hàm 	: LoadImg
+	// Mô tả	: Tải hình ảnh lên
+	// Kiểu trả về  : bool
+	// Tham số const char* file_name :  đường dẫn file
+
 	bool LoadImg(const char* file_name);
+
+	// Tên hàm 	: show
+	// Mô tả	: Thể hiện đối tượng lên cửa sổ
+	// Kiểu trả về  : void
+	// Tham số DL_Surface* des : Biến đích (thường là cửa sổ màn hình)
 	void show(SDL_Surface* des);
 
 	SDL_Rect GetRect() { return rect_; };
-	void SetRect(const int x, const int y,  int w = 0,  int h = 0) { rect_.x = x; rect_.y = y; rect_.w = w; rect_.h = h; }
+	void SetRect(int x, int y, int w = 0, int h = 0) { rect_.x = x; rect_.y = y; rect_.w = w; rect_.h = h; }
+
 	SDL_Surface* GetObject() { return  p_object_; }
 
 protected:
@@ -20,4 +32,3 @@ protected:
 	SDL_Rect rect_;
 	SDL_Surface* p_object_;
 };
-
