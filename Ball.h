8454d2ede30b802b9e16aee6b4d6838f82e928a0
nguyễn văn hải <48937890+vanhai28@users.noreply.github.com>
2019-11-12 16:54:16 +0700

    Update Ball.h

--- Ball.h
+++ Ball.h
@@ -6,7 +6,6 @@
 #define WIDTH_BALL 15
 #define HEIGHT_BALL 15
 #define MAX_SPEED 18
-//-------------------static variable---------------
 
 
 
@@ -17,13 +16,12 @@ public:
 	// Tên hàm 	: HandleMove
 	// Mô tả	: Di chuyển vị trí quả bóng
 	// Kiểu trả về  : void
-	// Tham số SDL_Rect playerTop : tọa độ thnah trượt 1, giúp xác định khi bóng va chạm
-	// Tham số SDL_Rect playerButtom : Tạo độ thanh trượt 2, giúp xác định khi bóng va chạm
+	// Tham số SDL_Rect playerTop : tọa độ thanh trượt 1, giúp xác định thời điểm bóng va chạm thanh trượt 1
+	// Tham số SDL_Rect playerButtom : Tạo độ thanh trượt 2, giúp xác định thời điểm bóng va chạm thanh trượt 2
 	// Tham số bool &is_finish : xác định trò chơi đã kết thúc chưa
-	// Tham số int &win : Xác định người chiến thắng khi bóng roi vào biên
-	// Tham số Mix_Chunk*& bullet_sound : Biến sử dụng tải âm thanh khi bóng cham thanh
-	// Tham số mark : Tăng điểm của người chơi trong màn chơi với máy
-
+	// Tham số int &win : Xác định người chiến thắng khi bóng rơi vào biên
+	// Tham số Mix_Chunk* sound[3] : Mảng các con trỏ sử dụng để truyền biến âm thanh vào trong hàm
+	// Tham số int& mark : Tăng điểm của người chơi trong màn chơi với máy
 	int HandleMove(SDL_Rect playerTop, SDL_Rect playerButtom, bool &is_finish, Mix_Chunk* sound[3], int& mark);
 
 	//------------- Các hàm Getter - Setter------------
