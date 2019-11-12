d57f6f253639edd4d9305a209bdbe263de45aa60
nguyễn văn hải <48937890+vanhai28@users.noreply.github.com>
2019-11-12 16:54:14 +0700

    Update AutoPlayer.h

--- AutoPlayer.h
+++ AutoPlayer.h
@@ -1,16 +1,20 @@
-﻿// Mô tả : định nghĩa lớp cho người chơi là máy tính
+﻿// Mô tả : định nghĩa lớp người chơi là máy tính
+// Ngày cập nhật : 01/11/2019
+
 #pragma once
-#include "Player1.h"
+#include "BasePlayer.h"
 #define x_step 10
-class AutoPlayer : public Player1
+class AutoPlayer : public BasePlayer
 {
 public:
 	AutoPlayer();
 	~AutoPlayer();
+
 	//--------------- Định nghĩa các hàm ảo ở lớp cơ sở -----------------
 	void PredictDropPoint(SDL_Rect ball, int x_val_ball, int y_val_ball);
 	void  HandleMove();
+	void HandleInputAction(SDL_Event events) {};
 
 private:
-	int x_des;//vj trí dự đoán quả bóng rơi
+	int x_des;//vị trí dự đoán quả bóng rơi
 };
